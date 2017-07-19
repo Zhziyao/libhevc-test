78cab21b825f2f2756cc9dd31f24aa2bd248b035
Zhziyao <ziyao.zhang@intel.com>
2017-07-19 10:09:38 +0800

    add tesh.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+#
+#this program is to set PATH for test.
+#
+#
+
+export PATH=/root/zzy/libavc-test:/root/zzy/libhevc-test:/root/zzy/libmpeg2-test:${PATH}
+
+echo "build test env PATH"
+
+echo 'the test PATH is ${PATH}'
---
+++ hevcfftest.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+
+#LIBHEVC TEST
+echo "hevcttest: -arc ssse3 core 1"
+printf "./libhevctest.py -lib h -loop 5 -core 1" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 1 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc ssse3 core 2"
+printf "./libhevctest.py -lib h -loop 5 -core 2" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 2 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc ssse3 core 4"
+printf "./libhevctest.py -lib h -loop 5 -core 4" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 4 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc sse42 core 1"
+printf "./libhevctest.py -lib h -loop 5 -core 1 -arc SSE42" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 1 -arc SSE42 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc sse42 core 2"
+printf "./libhevctest.py -lib h -loop 5 -core 2 -arc SSE42" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 2 -arc SSE42 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc sse42 core 4"
+printf "./libhevctest.py -lib h -loop 5 -core 4 -arc SSE42" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 4 -arc SSE42 >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc GENERIC core 1"
+printf "./libhevctest.py -lib h -loop 5 -core 1 -arc GENERIC" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 1 -arc GENERIC >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc GENERIC core 2"
+printf "./libhevctest.py -lib h -loop 5 -core 2 -arc GENERIC" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 2 -arc GENERIC >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+echo "hevcttest: -arc GENERIC core 4"
+printf "./libhevctest.py -lib h -loop 5 -core 4 -arc GENERIC" >>hevcres.txt
+./libhevctest.py -lib h -loop 5 -core 4 -arc GENERIC >>hevcres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
+
+#FFMPEG TEST
+echo "FFMPEG TEST"
+printf "./libhevctest.py -lib f -loop 5 " >>ffmpegres.txt
+./libhevctest.py -lib f -loop 5  >>ffmpegres.txt
+if [ $? -ne 0 ]; then
+	printf "last cmd run correctly"
+fi
