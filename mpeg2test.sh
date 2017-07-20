d000e8e89da7dd67b053fe61e8e0c261b84f0395
Zhziyao <ziyao.zhang@intel.com>
2017-07-20 15:33:10 +0800

    modify mpeg2

--- mpeg2test.sh
+++ mpeg2test.sh
@@ -7,63 +7,63 @@ if [ -f mpeg2.txt ]; then
 fi
 
 #LIBMPEG2 TEST
-echo "hevcttest: -arc ssse3 core 1"
+echo "mpeg2test: -arc ssse3 core 1"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 1" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 1 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc ssse3 core 2"
+echo "mpeg2test: -arc ssse3 core 2"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 2" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 2 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc ssse3 core 4"
+echo "mpeg2test: -arc ssse3 core 4"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 4" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 4 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc sse42 core 1"
+echo "mpeg2test: -arc sse42 core 1"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 1 -arc SSE42" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 1 -arc SSE42 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc sse42 core 2"
+echo "mpeg2test: -arc sse42 core 2"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 2 -arc SSE42" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 2 -arc SSE42 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc sse42 core 4"
+echo "mpeg2test: -arc sse42 core 4"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 4 -arc SSE42" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 4 -arc SSE42 >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc GENERIC core 1"
+echo "mpeg2test: -arc GENERIC core 1"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 1 -arc GENERIC" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 1 -arc GENERIC >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc GENERIC core 2"
+echo "mpeg2test: -arc GENERIC core 2"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 2 -arc GENERIC" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 2 -arc GENERIC >>mpeg2.txt
 if [ $? -ne 0 ]; then
 	printf "last cmd run correctly"
 fi
 
-echo "hevcttest: -arc GENERIC core 4"
+echo "mpeg2test: -arc GENERIC core 4"
 printf "./libmpeg2test.py -lib 2 -loop 5 -core 4 -arc GENERIC" >>mpeg2.txt
 ./libmpeg2test.py -lib 2 -loop 5 -core 4 -arc GENERIC >>mpeg2.txt
 if [ $? -ne 0 ]; then
